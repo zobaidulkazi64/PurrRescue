535b3c81e668552a5cdb11132a53d9c41fb7ee9b
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-04-18 23:56:56 +0600

    feat: user services controllers create user

---
+++ packages/services/user/src/controllers/getUserById.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from "express";
+
+// user by id
+
+const getUserById = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+  } catch (error) {}
+};
+
+export default getUserById;
