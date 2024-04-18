bbfce46892835600759c36c1be230f6a5bded8eb
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-04-19 04:45:06 +0600

    feat: user action add

--- packages/services/auth/src/controllers/index.ts
+++ packages/services/auth/src/controllers/index.ts
@@ -1,2 +1,3 @@
 export { default as UserRegistration } from "./registration";
 export { default as UserLogin } from "./login";
+export { default as authToken } from "./authToken";
--- packages/services/auth/src/routes/index.ts
+++ packages/services/auth/src/routes/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { UserRegistration, UserLogin } from "@/controllers";
+import { UserRegistration, UserLogin, authToken } from "@/controllers";
 
 router.post("/registration", UserRegistration);
 router.post("/login", UserLogin);
+router.post("/auth-token", authToken);
 
 export default router;
