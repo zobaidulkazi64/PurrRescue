e9478912a97e218309add73018dab2887a2af91f
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-04-21 23:53:57 +0600

    apiGeteway live

---
+++ packages/apiGateway/src/utils/schemas.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const UserCreateSchema = z.object({
+  authUserId: z.string(),
+  name: z.string(),
+  email: z.string().email(),
+  phoneNumber: z.string(),
+  address: z.string().default("Gazipur, Dhaka"),
+  city: z.string().default("Dhaka"),
+  state: z.string().default("Dhaka"),
+  country: z.string().default("Bangladesh"),
+  postalCode: z.string().default("1000"),
+  isActive: z.boolean().default(true),
+  isDeleted: z.boolean().default(false),
+});
+
+export const UserUpdateSchema = UserCreateSchema.omit({
+  authUserId: true,
+}).partial();
