cc11ff98cf1cffcf282125e87ab1320d99a1d99d
zobkazi <zobaidulkazi64.official@gmail.com>
2024-04-26 23:19:11 +0600

    pet details.1

--- packages/services/adoption/src/controllers/petDetails.ts
+++ packages/services/adoption/src/controllers/petDetails.ts
@@ -1,3 +1,20 @@
 import { Request, Response, NextFunction } from "express";
 import prisma from "@/utils/prisma";
 import { CreatePetSchema } from "@/utils/schemas";
+
+
+const petDetails = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // req.params
+        const { id } = req.params;
+
+        const pet = await prisma.pet.findFirst({
+            where: {
+                id,
+            },
+        
+        })
+    } catch (error) {
+        next(error);
+    }
+}
