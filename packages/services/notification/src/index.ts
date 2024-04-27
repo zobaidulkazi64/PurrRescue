4a6f3541bb9288d676bac27cf39046563a364236
zobkazi <zobaidulkazi64.official@gmail.com>
2024-04-28 04:22:29 +0600

    ex: notify server added

--- packages/services/notification/src/index.ts
+++ packages/services/notification/src/index.ts
@@ -1,1 +1,15 @@
 // express server for notification service
+
+import express from "express";
+
+const app = express();
+
+app.get("/health", (_req, res) => {
+    res.status(200).json({ status: "UP" });
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+export default app
