8afdf3db9f89efbc70c88b0c8f11db1b31872a28
zobkazi <zobaidulkazi64.official@gmail.com>
2024-04-28 23:58:07 +0600

    index

--- packages/services/email/src/controllers/sendEmail.ts
+++ packages/services/email/src/controllers/sendEmail.ts
@@ -2,3 +2,19 @@ import { Request, Response, NextFunction } from 'express';
 import prisma from '@/utils/prisma';
 import { EmailCreateSchema } from '@/utils/schemas';
 import { defaultSender, transporter } from '@/config';
+
+
+
+
+const sendEmail = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // validate input the req body
+        const parsedBody = EmailCreateSchema.safeParse(req.body)
+
+        if (!parsedBody.success) {
+            return res.status(400).json(parsedBody.error.errors)
+        }
+    } catch (error) {
+        next(error)
+    }
+}
