8a2cc4693270752f24ab994d3f121af4b43575d3
zobaidul kazi <zobaidulkazi64.official@gmail.com>
2024-05-21 09:51:03 +0600

    email and email receive bode successfully

--- packages/services/email/src/EmailBody.ts
+++ packages/services/email/src/EmailBody.ts
@@ -4,7 +4,10 @@ const emailBody = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { email } = req.body;
 
-    console.log(email);
+    res.status(200).json({
+      message: "Email body fetched successfully",
+      data: email,
+    });
   } catch (error) {
     next(error);
   }
--- packages/services/email/src/utils/receiver.ts
+++ packages/services/email/src/utils/receiver.ts
@@ -1,7 +1,42 @@
 import amqp from "amqplib";
-import {defaultSender, transporter} from '@/config'
+import { defaultSender, transporter } from "@/config";
 
+const receiveFromQueue = async (
+  queue: string,
+  callback: (message: string) => void
+) => {
+  try {
+    const connection = await amqp.connect("amqp://localhost");
 
-const receiveFromQueue = async (queue: string, callback: (message: string) => void) => {
-    
-}
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(queue);
+
+    channel.consume(queue, (message) => {
+      if (message) {
+        callback(message.content.toString());
+      }
+
+      channel.ack(message);
+
+      connection.close();
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const sendToQueue = async (queue: string, message: string) => {
+  try {
+    const connection = await amqp.connect("amqp://localhost");
+    const channel = await connection.createChannel();
+    await channel.assertQueue(queue);
+    await channel.sendToQueue(queue, Buffer.from(message));
+    await channel.close();
+    await connection.close();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default receiveFromQueue;
