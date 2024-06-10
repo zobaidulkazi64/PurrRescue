3c00fc04c9e9571cdfafb039aa1d969e2e1bfd51
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-06-10 19:30:04 +0600

    auth tocken is req

--- packages/services/auth/src/controllers/authToken.ts
+++ packages/services/auth/src/controllers/authToken.ts
@@ -1,14 +1,41 @@
-import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
-import prisma from "@/utils/prisma";
-import { AccessTokenSchema } from "@/utils/schemas";
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import prisma from '@/utils/prisma';
+import { AccessTokenSchema } from '@/utils/schemas';
 
 const authToken = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    // Validate the request body
-    const accessToken = req.headers.authorization?.split(" ")[1];
+    const accessToken = req.headers.authorization?.split(' ')[1];
 
-    console.log(accessToken);
+    if (!accessToken) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const parsedBody = AccessTokenSchema.safeParse(accessToken);
+    if (!parsedBody.success) {
+      return res.status(400).json({
+        success: false,
+        error: parsedBody.error,
+      });
+    }
+
+    const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET!);
+    if (!decoded) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const user = await prisma.authUser.findUnique({
+      where: {
+        id: (decoded as any).id,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    req.user = user;
+    next();
   } catch (error) {
     next(error);
   }
